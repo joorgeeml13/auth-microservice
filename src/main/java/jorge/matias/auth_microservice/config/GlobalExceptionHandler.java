a264259ccc06e33cf68d22807a0e39def3d69108
Jorge Matias <jorge29364767@gmail.com>
2026-06-08 13:42:48 +0200

    Cambio exception handler

--- src/main/java/jorge/matias/auth_microservice/config/GlobalExceptionHandler.java
+++ src/main/java/jorge/matias/auth_microservice/config/GlobalExceptionHandler.java
@@ -2,11 +2,14 @@ package jorge.matias.auth_microservice.config;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -35,15 +38,43 @@ public class GlobalExceptionHandler {
                 LocaleContextHolder.getLocale() 
         );
 
+        return buildResponse(ex.getHttpStatus(), Constantes.AUTH_ERROR_CODE, translatedMessage, request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleValidationException(
+        MethodArgumentNotValidException ex,
+        HttpServletRequest request
+    ){
+        String errors = ex.getBindingResult().getFieldErrors().stream()
+            .map(error -> {
+                String message = error.getDefaultMessage();
+
+                return messageSource.getMessage(
+                    message != null ? message : "error.bad_request",
+                    null,
+                    message,
+                    LocaleContextHolder.getLocale()
+                );
+            })
+            .collect(Collectors.joining(" | "));
+
+            return buildResponse(HttpStatus.BAD_REQUEST, Constantes.VALIDATION_ERROR_CODE, errors, request);
+    }
+
+    private ResponseEntity<ApiErrorResponse> buildResponse(
+        HttpStatus status, 
+        String code, 
+        String message, 
+        HttpServletRequest request
+    ) {
         ApiErrorResponse res = new ApiErrorResponse(
             LocalDateTime.now(),
-            ex.getHttpStatus().value(),
-            Constantes.AUTH_ERROR_CODE,
-            translatedMessage,
+            status.value(),
+            code,
+            message,
             request.getRequestURI()
-            
         );
-
-        return ResponseEntity.badRequest().body(res);
+        return ResponseEntity.status(status).body(res);
     }
 }

57110e280e1b9d08e36f284a220b59c8a6232e01
Jorge Matias <jorge29364767@gmail.com>
2026-07-29 19:31:13 +0200

    fix test

--- src/test/java/jorge/matias/auth_microservice/AbstractIntegrationTest.java
+++ src/test/java/jorge/matias/auth_microservice/AbstractIntegrationTest.java
@@ -2,23 +2,23 @@ package jorge.matias.auth_microservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
+@SpringBootTest
 public abstract class AbstractIntegrationTest {
 
     @Value("${server.servlet.context-path}")
     protected String contextPath;
     
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:15-alpine"
+    );
+
+    static {
+        postgres.start();
+    }
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
